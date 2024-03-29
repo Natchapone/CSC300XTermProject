5eda946970c3cdf8ecee7bd9fa7875144334bcc6
Jayden <jecathcart@uncg.edu>
2024-03-28 21:27:09 -0400

    SQLs

---
+++ create_tables.sql
@@ -0,0 +1,45 @@
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "categories" (
+	"catID"	INTEGER,
+	"cat_name"	TEXT NOT NULL,
+	"displayOrder"	INTEGER NOT NULL,
+	PRIMARY KEY("catID" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "cartProducts" (
+	"cpID"	INTEGER,
+	"cartID"	INTEGER NOT NULL UNIQUE,
+	"productID"	INTEGER NOT NULL UNIQUE,
+	"quantity"	INTEGER NOT NULL,
+	PRIMARY KEY("cpID" AUTOINCREMENT),
+	FOREIGN KEY("cartID") REFERENCES "carts"("cartID"),
+	FOREIGN KEY("productID") REFERENCES "products"("productID")
+);
+CREATE TABLE IF NOT EXISTS "carts" (
+	"cartID"	INTEGER,
+	"status"	TEXT NOT NULL,
+	"created"	NUMERIC NOT NULL,
+	"userID"	INTEGER NOT NULL UNIQUE,
+	PRIMARY KEY("cartID" AUTOINCREMENT),
+	FOREIGN KEY("userID") REFERENCES "users"("userID")
+);
+CREATE TABLE IF NOT EXISTS "products" (
+	"productID"	INTEGER,
+	"product_name"	TEXT NOT NULL,
+	"description"	TEXT NOT NULL,
+	"imagepath"	TEXT NOT NULL,
+	"price"	REAL NOT NULL,
+	"catID"	INTEGER NOT NULL,
+	"featured"	BLOB,
+	PRIMARY KEY("productID" AUTOINCREMENT),
+	FOREIGN KEY("catID") REFERENCES "categories"("catID")
+);
+CREATE TABLE IF NOT EXISTS "users" (
+	"userID"	INTEGER,
+	"username"	TEXT NOT NULL UNIQUE,
+	"email"	TEXT NOT NULL UNIQUE,
+	"password"	TEXT NOT NULL,
+	"userType"	TEXT NOT NULL,
+	"created"	NUMERIC NOT NULL,
+	PRIMARY KEY("userID" AUTOINCREMENT)
+);
+COMMIT;
---
+++ drop_tables.sql
@@ -0,0 +1,5 @@
+Drop TABLE users;
+Drop TABLE categories;
+Drop TABLE products;
+Drop TABLE carts;
+Drop TABLE cartProducts;
