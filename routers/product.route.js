4ff45b1037b8880ee59d2553fb41b293a16cd6af
Natchapone Janjon <n_janjon@uncg.edu>
2024-04-12 19:38:00 -0400

    intitial router

---
+++ routers/product.route.js
@@ -0,0 +1,16 @@
+"use strict";
+const express = require("express");
+const router = express.Router();
+
+const productcontroller = require("../controllers/product.controller");
+
+router.get("/products", productcontroller.getAll); // route to the all products page
+router.get("/category/:category", productcontroller.getAllByCategory); // route to the product page by category
+router.get("/item/:id", productcontroller.getOneById); // route to a specific product
+router.post("/add", productcontroller.createNew); // action to add new product in the product edit page
+router.get("/search", productcontroller.searchByName); // 
+router.delete("/delete/:id", productcontroller.deleteById); // action to delete a product in the product edit page
+router.put("/edit/:id", productcontroller.update); // action to edit product on the edit page
+router.get("/cart", productcontroller.getCartItems); // route to the cart page
+
+module.exports = router;
