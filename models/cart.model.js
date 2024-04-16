5c3917f0462e6b01fc00b7726a87846c2c7c7e45
Jayden <jecathcart@uncg.edu>
2024-04-16 01:36:22 -0400

    cart controller and model not working

--- models/cart.model.js
+++ models/cart.model.js
@@ -1,4 +1,29 @@
 "use strict";
 const db = require("../models/db-conn");
 
-//function addToCart(
+function createCart(userID) {
+    let sql = "INSERT INTO carts (status, created, userID) VALUES (?,?,?);";
+    let status = "active";
+    let created = new Date().toLocaleDateString('en-US');
+    const params = [status, created, userID];
+    const cart = db.run(sql, params);
+    return cart.lastInsertRowid();
+}
+
+function addToCart(cartID, productID, quantity) {
+    let sql = "INSERT INTO cartProducts (cartID, productID, quantity) VALUES (?, ?, ?);";
+    const params = [cartID, productID, quantity];
+    db.run(sql, params);
+}
+
+function getCartProducts(cartID) {
+    let sql = "SELECT cp.*, p.product_name, p.description, p.imagepath, p.price FROM cartProducts cp INNER JOIN products p ON cp.productID = p.productID WHERE cp.cartID = ?;";
+    return db.all(sql, cartID);
+}
+
+
+module.exports = {
+    createCart,
+    addToCart,
+    getCartProducts,
+};
--- routes/cart.route.js
+++ routes/cart.route.js
@@ -3,6 +3,8 @@ const express = require("express");
 const router = express.Router();
 const cartController = require("../controllers/cart.controller");
 
+router.post("/", cartController.createCart);
 router.post("/add", cartController.addToCart);
+router.get("/cart", cartController.getCart);
 
 module.exports = router;
